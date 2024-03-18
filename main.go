cc61c7ae92549d5130037c3e8686ee4bdde3c92c
Alex.lin <alex.lin@letron.tech>
2024-03-18 17:56:40 +0800

    add Switch

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -14,24 +15,35 @@ var (
 		Name: "connection_numbers",
 		Help: "Number of connections from API",
 	})
-	alex_test = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "alex_numbers",
-		Help: "Alex test metrics",
-	})
+	// alex_test = prometheus.NewGauge(prometheus.GaugeOpts{
+	// 	Name: "alex_numbers",
+	// 	Help: "Alex test metrics",
+	// })
 )
 
 func init() {
 	log.SetFlags(0)
 	prometheus.MustRegister(connectionNumbers)
-	prometheus.MustRegister(alex_test)
+	// prometheus.MustRegister(alex_test)
 }
 
 func main() {
 	log.Printf("[%s] [INFO] Starting the application...", time.Now().Format(time.RFC3339))
-	go fetchMetrics()
-	go fetchJsonUrlMetrics()
-	go fetchFileMetrics()
-	alex_test.Set(123456)
+	METRICS_JSON := os.Getenv("METRICS_JSON")
+	METRICS_URL := os.Getenv("METRICS_URL")
+	METRICS_FILE := os.Getenv("METRICS_FILE")
+	if METRICS_JSON != "disable" {
+		go fetchJsonUrlMetrics()
+	}
+
+	if METRICS_URL != "disable" {
+		go fetchMetrics()
+	}
+
+	if METRICS_FILE != "disable" {
+		go fetchFileMetrics()
+	}
+	// alex_test.Set(123456)
 	http.Handle("/metrics", promhttp.Handler())
 	log.Printf("[%s] [INFO] HTTP server started on :8080", time.Now().Format(time.RFC3339))
 	http.ListenAndServe(":8080", nil)
